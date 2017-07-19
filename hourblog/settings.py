c903662035d65eba5c9dd495bfa90220b03e1528
Bikram Bharti <bikram.bharti.ece15@itbhu.ac.in>
2017-07-20 01:00:33 +0530

    techwana.herokuapp.com added to allowed hosts

--- hourblog/settings.py
+++ hourblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+!tq9rlxr16tu=#265#ipg40zbw5c*+do@i)*d(4&9clw7-^_f'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['techwana.herokuapp.com']
 
 
 # Application definition
